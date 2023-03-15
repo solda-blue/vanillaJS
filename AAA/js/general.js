45d3a74f3f76a5180e344bc8ea36ff83e075ae56
solda <principe1513@gmail.com>
2023-03-15 16:42:04 +0900

    일단 카카오 로그인 로그아웃

--- AAA/js/general.js
+++ AAA/js/general.js
@@ -1,15 +1,26 @@
 function handleToken() {
-        let token = sessionStorage.getItem("TOKEN");
-        return new Promise ((resolve, reject) => {
-            fetch(`http://127.0.0.1:8088/member/selectone.json`, {
-                method : "GET",
-                headers : {
-                    "token" : token
-                }
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                resolve(data);
-            })
+    let token = sessionStorage.getItem("TOKEN");
+    return new Promise ((resolve, reject) => {
+        fetch(`http://127.0.0.1:8088/member/selectone.json`, {
+            method : "GET",
+            headers : {
+                "token" : token
+            }
         })
-}
+        .then((response) => response.json())
+        .then((data) => {
+            resolve(data);
+        })
+    })
+};
+
+// import kakao from "https://developers.kakao.com/sdk/js/kakao.js";
+// function handleKakaoToken() {
+//     window.Kakao.API.request({
+//         url : '/v2/user/me',
+//         success : res => {
+//             const kakao_account = res.kakao_account;
+//             // console.log('kakao_account => ', kakao_account);
+//         }
+//     });
+// };
